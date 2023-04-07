6e2c0bc080f432081000f8a66f60720d1a08543b
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-04-07 20:16:30 +0300

    Add used languages to tree sitter ensure installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "javascript", "typescript", "ruby", "rust", "json", "yaml", "dockerfile", "html",
+      "markdown", "graphql", "proto", "python", "regex", "sql", "tsx", "css", "scss" },
   },
 }
