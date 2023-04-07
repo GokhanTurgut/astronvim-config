56a89e25ff0f8903c767a5807c70c7e98961b291
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-04-07 20:17:02 +0300

    Change neo tree show hidden files by default

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -2,6 +2,9 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, config)
     config.window.width = 42 -- Meaning of everything :)
+    config.filesystem.filtered_items = {
+      visible = true -- Show hidden files by default
+    }
     return config
   end,
 }
