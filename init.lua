c039bbe17ebe8c009ad6bfa46aea8a4eed5ffedc
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-04-24 15:11:53 +0300

    Change color scheme due to deprecation

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "github_dark_default",
+  colorscheme = "github_dark",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
