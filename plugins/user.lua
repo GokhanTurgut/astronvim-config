af889484fb1d8ec556207982d13b31eca6c6a7e5
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-04-30 00:35:53 +0300

    Add markdown-preview plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -23,4 +23,11 @@ return {
       require("tokyonight").setup {}
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    event = "BufRead",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
