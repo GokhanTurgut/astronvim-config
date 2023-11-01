8ca4d7cfb0734457a41c72a7303f274cc504c463
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-01 03:46:09 +0300

    Add nvim-spectre plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,4 +4,5 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
+  { import = "astrocommunity.project.nvim-spectre" },
 }
