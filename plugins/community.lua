dfce0775384e54d01eb0b894fd95b14af673a5bf
Gokhan Turgut <gokhan.turgut@gusto.com>
2023-11-03 17:04:29 +0300

    Add nvim-surround plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,4 +5,5 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
   { import = "astrocommunity.project.nvim-spectre" },
+  { import = "astrocommunity.motion.nvim-surround" },
 }
